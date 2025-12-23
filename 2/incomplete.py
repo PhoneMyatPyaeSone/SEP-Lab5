539a04104e4fd16816b8cc0b8bd64b2c281b5b1f
San Aung <165061646+Sanaunggithub@users.noreply.github.com>
2025-12-23 17:53:52 +0700

    Update incomplete.py
    Added cost feature

--- 2/incomplete.py
+++ 2/incomplete.py
@@ -44,19 +44,19 @@ class Train(Transportation):
       return 5 * self.stations
 
 
-# main program
+# Team Member 3
 
-# travel_cost = 0
+travel_cost = 0
 
-# trip = [Walk("KMITL", "KMITL SCB Bank", 0.6),
-#         Taxi("KMITL SCB Bank", "Ladkrabang Station", 5),
-#         Train("Ladkrabang Station", "Payathai Station", 40, 6),
-#         Taxi("Payathai Station", "The British Council", 3)]
+trip = [Walk("KMITL", "KMITL SCB Bank", 0.6),
+        Taxi("KMITL SCB Bank", "Ladkrabang Station", 5),
+        Train("Ladkrabang Station", "Payathai Station", 40, 6),
+        Taxi("Payathai Station", "The British Council", 3)]
 
-# for travel in trip:
-#    travel_cost += travel.find_cost()
+for travel in trip:
+   travel_cost += travel.find_cost()
 
-# print("Total travel cost:", travel_cost, "Bahts")
+print("Total travel cost:", travel_cost, "Bahts")
 
 # Display detailed breakdown
 # print("\nTrip Breakdown:")
